3d19c094757767fca5025c81280bfbf08f766b24
Marianne Umandal <marianneumandal209@gmail.com>
2026-09-10 10:00:03 +0800

    Fix base_url config with https protocol

--- app/config/config.php
+++ app/config/config.php
@@ -79,7 +79,7 @@ $config['environment'] = getenv('APP_ENV') ?: 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url']                 = 'umandal-marianne.onrender.com';
+$config['base_url']                 = 'https://umandal-marianne.onrender.com/';
 
 /*
 |--------------------------------------------------------------------------
